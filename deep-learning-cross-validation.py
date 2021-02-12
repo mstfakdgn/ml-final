1ef3eba6256b0e52d512ae86cba90e79c4ca62a6
flyiva-05 <m_akdogan@anadolu.edu.tr>
2021-02-12 15:43:08 +0300

    asasd

--- deep-learning-cross-validation.py
+++ deep-learning-cross-validation.py
@@ -10,14 +10,17 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from keras.optimizers import SGD
 from sklearn.metrics import accuracy_score
 
-def create_model(learn_rate=0.01, momentum=0):
+def create_model(learn_rate=0.1, epochs = 150, batch_size=32, optimizer="rmsprop"):
     
 	model = Sequential()
 	model.add(Dense(12, input_dim=9, activation='relu'))
+	# model.add(Dense(512,kernel_initializer=init))
+	# model.add(Dense(10,kernel_initializer=init))
 	model.add(Dense(1, activation='sigmoid'))
 	
-	optimizer = SGD(lr=learn_rate, momentum=momentum)
+	# optimizer = SGD(lr=learn_rate, momentum=momentum)
 	model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
 	return model
 
 seed = 7
@@ -34,27 +37,32 @@ y = df.iloc[:, 10]
 X = X.astype('float32')
 y = LabelEncoder().fit_transform(y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
-model = KerasClassifier(build_fn=create_model, epochs=100, batch_size=10, verbose=0)
+model = create_model()
+model.fit(X_train, y_train)
 
-# params = {
-#     "learn_rate" : np.linspace(0.1, 0.000001, num=10),
-# }
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print('Accuracy: %f' % (accuracy))
+print('Loss: %f' % (loss))
 
-# # define the grid search parameters
-# params = {
-#     "learn_rate" : [0.001, 0.01, 0.1, 0.2, 0.3],
-#     "momentum" : [0.0, 0.2, 0.4, 0.6, 0.9, 0.9]
-# }
+model = KerasClassifier(build_fn=create_model)
 
-# model_cv = GridSearchCV(estimator=model, param_grid=params, n_jobs=-1, cv=10, verbose=2).fit(X_train, y_train)
+params = {
+    "learn_rate" : [1E-0, 1E-1, 1E-2, 1E-3, 1E-4, 1E-5, 1E-6, 1E-7, 1E-8, 1E-9],
+	"epochs" : [50, 100, 150],
+	"batch_size" : [5, 10, 20],
+	"optimizer" : ['rmsprop', 'adam'],
+	# "init" : ['glorot_uniform', 'normal', 'uniform']
+}
 
-# print("Best: %f using %s" % (model_cv.best_score_, model_cv.best_params_))
+model_cv = GridSearchCV(estimator=model, param_grid=params, n_jobs=-1, cv=5, verbose=2).fit(X_train, y_train)
 
-tuned_model = create_model(learn_rate=0.01, momentum=0.6)
+print("Best: %f using %s" % (model_cv.best_score_, model_cv.best_params_))
+
+tuned_model = create_model(batch_size=10, epochs=50, learn_rate=1e-07, optimizer='rmsprop')
 tuned_model.fit(X_train, y_train)
 
 loss, accuracy = tuned_model.evaluate(X_test, y_test, verbose=0)
-print('Accuracy: %f' % (accuracy))
-print('Loss: %f' % (loss))
+print('Tuned  Accuracy: %f' % (accuracy))
+print('Tuned Loss: %f' % (loss))
