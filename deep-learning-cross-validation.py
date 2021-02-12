599ce6d7a952178f0ed77cb9678b5846c104d40e
flyiva-05 <m_akdogan@anadolu.edu.tr>
2021-02-12 16:24:20 +0300

    best params

--- deep-learning-cross-validation.py
+++ deep-learning-cross-validation.py
@@ -10,12 +10,12 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from keras.optimizers import SGD
 from sklearn.metrics import accuracy_score
 
-def create_model(learn_rate=0.1, epochs = 150, batch_size=32, optimizer="rmsprop"):
+def create_model(learn_rate=0.1, epochs = 150, batch_size=32, optimizer="rmsprop",init="glorot_uniform"):
     
 	model = Sequential()
 	model.add(Dense(12, input_dim=9, activation='relu'))
-	# model.add(Dense(512,kernel_initializer=init))
-	# model.add(Dense(10,kernel_initializer=init))
+	model.add(Dense(512,kernel_initializer=init))
+	model.add(Dense(10,kernel_initializer=init))
 	model.add(Dense(1, activation='sigmoid'))
 	
 	# optimizer = SGD(lr=learn_rate, momentum=momentum)
@@ -46,21 +46,21 @@ loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
 print('Accuracy: %f' % (accuracy))
 print('Loss: %f' % (loss))
 
-model = KerasClassifier(build_fn=create_model)
+# model = KerasClassifier(build_fn=create_model)
 
-params = {
-    "learn_rate" : [1E-0, 1E-1, 1E-2, 1E-3, 1E-4, 1E-5, 1E-6, 1E-7, 1E-8, 1E-9],
-	"epochs" : [50, 100, 150],
-	"batch_size" : [5, 10, 20],
-	"optimizer" : ['rmsprop', 'adam'],
-	# "init" : ['glorot_uniform', 'normal', 'uniform']
-}
+# params = {
+#     "learn_rate" : [1E-0, 1E-1, 1E-2, 1E-3, 1E-4, 1E-5, 1E-6, 1E-7, 1E-8, 1E-9],
+# 	"epochs" : [50, 100, 150],
+# 	"batch_size" : [5, 10, 20],
+# 	"optimizer" : ['rmsprop', 'adam'],
+# 	"init" : ['glorot_uniform', 'normal', 'uniform']
+# }
 
-model_cv = GridSearchCV(estimator=model, param_grid=params, n_jobs=-1, cv=5, verbose=2).fit(X_train, y_train)
+# model_cv = GridSearchCV(estimator=model, param_grid=params, n_jobs=-1, cv=3, verbose=2).fit(X_train, y_train)
 
-print("Best: %f using %s" % (model_cv.best_score_, model_cv.best_params_))
+# print("Best: %f using %s" % (model_cv.best_score_, model_cv.best_params_))
 
-tuned_model = create_model(batch_size=10, epochs=50, learn_rate=1e-07, optimizer='rmsprop')
+tuned_model = create_model(batch_size=10, epochs=50, init='normal', learn_rate=1e-07, optimizer='rmsprop')
 tuned_model.fit(X_train, y_train)
 
 loss, accuracy = tuned_model.evaluate(X_test, y_test, verbose=0)
